163e4ef95a14271b116bbfaba8399460e3a2a804
YtMa <ytma98@gmail.com>
2019-08-22 10:10:33 +0800

    :cat: Implement Basic Cat Displayer Function

--- extension.js
+++ extension.js
@@ -1,35 +1,116 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const axios = require('axios');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let enabled = false;
+let count = 0;
+let panel = null;
+let documentChangeListenerDisposer = null;
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-cat" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+function activate() {
+	vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration);
+	onDidChangeConfiguration();
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	count = 0;
+
+	if (panel) {
+		panel.dispose();
+	}
+
+	if (documentChangeListenerDisposer) {
+		documentChangeListenerDisposer.dispose();
+		documentChangeListenerDisposer = null;
+	}
+}
+
+function onDidChangeConfiguration() {
+	const config = vscode.workspace.getConfiguration('codecat');
+	const oldEnabled = enabled;
+	enabled = config.get('enabled', false);
+
+	if (!oldEnabled && enabled) {
+		init();
+		return;
+	}
+
+	if (oldEnabled && !enabled) {
+		deactivate();
+		return;
+	}
+
+	if (!enabled) {
+		return;
+	}
+}
+
+function init() {
+	deactivate();
+	count = 0;
+	initPanel();
+	documentChangeListenerDisposer = vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
+}
+
+function initPanel() {
+	panel = vscode.window.createWebviewPanel('codecat', 'Code? Cat!', vscode.ViewColumn.Beside, {});
+	panel.onDidDispose(() => { panel = null; });
+	panel.webview.html = `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Code? Cat!</title>
+			</head>
+			<body>
+				<h1>Keep Coding!</h1>
+			</body>
+		</html>
+	`
+}
+
+function onDidChangeTextDocument() {
+	count++;
+	if (count >= 100) {
+		count = 0;
+		if (!panel) {
+			initPanel();
+		}
+		panel.webview.html = `
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<meta charset="UTF-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<title>Code? Cat!</title>
+				</head>
+				<body>
+					<h1>Loading Cats...</h1>
+				</body>
+			</html>
+		`;
+		axios.get('https://api.thecatapi.com/v1/images/search', {
+			headers: {'x-api-key': 'b327b964-1f11-4080-a32a-fe8c265237e9'}
+		})
+			.then((res) => {
+				panel.webview.html = `
+				<!DOCTYPE html>
+				<html>
+					<head>
+						<meta charset="UTF-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1.0">
+						<title>Code? Cat!</title>
+					</head>
+					<body>
+						<img src="${res.data[0].url}" width=${res.data[0].width} height=${res.data[0].height} />
+					</body>
+				</html>
+				`;
+				panel.reveal();
+			})
+			.catch(console.log);
+	}
+}
 
 module.exports = {
 	activate,
